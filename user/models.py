7fa00ddee188d21a3937e523dfbad075da17289c
ryad boulahrouf <yoruzuya84@hotmail.fr>
2023-02-07 20:16:19 +0100

    first_commit

---
+++ user/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class MyUser(AbstractUser):
+
+    username = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["username"], name="unique user")]
+
+
+# Create your models here.
