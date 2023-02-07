18e53b9301df221c39c0c896eae9c91c3a7c83dd
ryad boulahrouf <yoruzuya84@hotmail.fr>
2023-02-07 20:54:19 +0100

    authentification user and admin

--- SoftDesk/urls.py
+++ SoftDesk/urls.py
@@ -17,11 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from user.views import MyUserAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = routers.SimpleRouter()
 
 router.register('signup', MyUserAPIView, basename='signup')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from user.models import MyUser
+
+class MyUserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'password')
+
+admin.site.register(MyUser)
 
 # Register your models here.
